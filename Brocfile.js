4990f0efe09e3e4148b5bb1370a70b6fdf94ea4c
Anthony Sherbondy <ts1235813@gmail.com>
2015-01-21 14:27:21 -0800

    Add fontawesome

--- Brocfile.js
+++ Brocfile.js
@@ -5,7 +5,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp({
   sassOptions: {
     includePaths: [
-      'bower_components/bootstrap-sass-official/assets/stylesheets/'
+      'bower_components/bootstrap-sass-official/assets/stylesheets/',
+      'bower_components/fontawesome/scss'
     ]
   }
 });
