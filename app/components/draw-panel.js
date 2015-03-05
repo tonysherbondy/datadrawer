b505cbe07f6f7d39b51cc6c0382ef1615fb66f1e
Anthony Sherbondy <ts1235813@gmail.com>
2015-03-04 23:23:16 -0800

    Can eval some d3 code

--- app/components/draw-panel.js
+++ app/components/draw-panel.js
@@ -1,5 +1,20 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ['draw-panel']
+  classNames: ["draw-panel"],
+
+  didInsertElement: function() {
+    this.draw();
+  },
+
+  selectChart: function() {
+    return d3.select(this.$(".chart")[0]);
+  },
+
+  draw: function() {
+    this.selectChart().remove();
+    d3.select("svg").append("g").attr("class", "chart");
+
+    eval(this.get("d3Code"));
+  }.observes("d3Code")
 });
