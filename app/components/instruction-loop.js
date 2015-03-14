a4dc980a72139176b5169c175abac1e91fa26363
Nhan Nguyen <nhan@alum.mit.edu>
2015-03-13 17:43:19 -0700

    Fixed bug with setting loop variable on instruction

--- app/components/instruction-loop.js
+++ app/components/instruction-loop.js
@@ -7,6 +7,20 @@ export default Ember.Component.extend({
     });
   }).property("dataItems"),
 
-  loopVariable: Ember.computed.oneWay("selectedLoopData.name")
 
+  // we want to propagate this value to instruction.loopVariable
+  // default to selectedLoopData.name
+  // unless the loop variable has been specified
+  loopVariable: function(key, value, previous) {
+    if (arguments.length > 1) {
+      this.set("instruction.loopVariable", value);
+    }
+
+    if (!!this.get("instruction.loopVariable")) {
+      return this.get("instruction.loopVariable");
+    }
+
+    return this.get("selectedLoopData.name");
+
+  }.property("selectedLoopData.name", "instruction.loopVariable")
 });
