6a0deed8bc61eca0f0d86f5b2547f804d6452a95
Anthony Sherbondy <ts1235813@gmail.com>
2015-03-18 11:43:38 -0600

    Scatterplot also and refactor mark drawing.

--- app/routes/marks.js
+++ app/routes/marks.js
@@ -5,10 +5,35 @@ var Table = Ember.Object.extend({
 });
 
 var Mark = Ember.Object.extend({
-  table: Ember.required(),
   displayString: function() {
     return "Empty Mark";
+  },
+  getAttrFuncD3: function(attr) {
+    if (!this.get(attr)) {
+      return null;
+    }
+    return `function(element, index) { return ${this.get(attr + '.stringRepresentation')}; }`;
+  },
+
+  getD3DrawPrefix: function(type) {
+    return `this.selectChart().selectAll('${type}').data(table).enter().append('${type}')`;
+  },
+
+  getD3Attrs: function(attrsMap) {
+    return "\n" + attrsMap.map((item) => {
+      let attrFunc = this.getAttrFuncD3(item.name);
+      if (!attrFunc) {
+        return "";
+      }
+      return `.attr('${item.d3Name}', ${attrFunc})`;
+    }).join("\n");
+  },
+
+  getD3Code: function() {
+    var attrsMap = this.get("attrsMap");
+    return this.getD3DrawPrefix(this.get("type")) + this.getD3Attrs(attrsMap) + ";";
   }
+
 });
 
 var Expression = Ember.Object.extend({
@@ -27,6 +52,8 @@ function e(stringRep) {
 }
 
 var RectangleMark = Mark.extend({
+  type: "rect",
+
   width: Ember.required(),
   height: Ember.required(),
   top: Ember.required(),
@@ -43,21 +70,45 @@ var RectangleMark = Mark.extend({
 
   }.property("width", "height", "top", "left"),
 
-  getAttrFuncD3: function(attr) {
-    return `function(element, index) { return ${this.get(attr + '.stringRepresentation')}; }`;
-  },
+  attrsMap: [
+    {name: "width", d3Name: "width"},
+    {name: "height", d3Name: "height"},
+    {name: "left", d3Name: "x"},
+    {name: "top", d3Name: "y"},
+    {name: "fill", d3Name: "fill"}
+  ],
 
   d3Code: function() {
-    var widthFunc = this.getAttrFuncD3("width");
-    var heightFunc = this.getAttrFuncD3("height");
-    var leftFunc = this.getAttrFuncD3("left");
-    var topFunc = this.getAttrFuncD3("top");
-    return `this.selectChart().selectAll('rect').data(table).enter().append('rect')
-    .attr('height', ${heightFunc})
-    .attr('x', ${leftFunc})
-    .attr('y', ${topFunc})
-    .attr('width', ${widthFunc});`;
-  }.property("width", "height", "top", "left")
+    return this.getD3Code();
+  }.property("width", "height", "top", "left", "fill")
+});
+
+var CircleMark = Mark.extend({
+  type: "circle",
+
+  radius: Ember.required(),
+  cx: Ember.required(),
+  cy: Ember.required(),
+
+  displayString: function() {
+    var radius = this.get("radius.stringRepresentation");
+    var cy = this.get("cy.stringRepresentation");
+    var cx = this.get("cx.stringRepresentation");
+
+    return `draw cirlce at [${cx}], [${cy}] with radius ${radius}`;
+  }.property("radius", "cx", "cy"),
+
+  attrsMap: [
+    {name: "radius", d3Name: "r"},
+    {name: "cx", d3Name: "cx"},
+    {name: "cy", d3Name: "cy"},
+    {name: "fill", d3Name: "fill"}
+  ],
+
+  d3Code: function() {
+    return this.getD3Code();
+  }.property("radius", "cx", "cy", "fill")
+
 });
 
 
@@ -66,7 +117,7 @@ var RectangleMark = Mark.extend({
 var MarksToD3Compiler = Ember.Object.extend({
   marks: Ember.required(),
   d3Code: function() {
-    return this.get("marks.firstObject.d3Code");
+    return this.get("marks").getEach("d3Code").join("\n");
   }.property("marks.[]")
 });
 
@@ -74,9 +125,9 @@ export default Ember.Route.extend({
   model: function() {
     var table =  Table.create({
       rows: [
-        {name: "Nhan", age: 27},
-        {name: "Zack", age: 30},
-        {name: "Anthony", age: 37}
+        {name: "Nhan", age: 27, weight: 120},
+        {name: "Zack", age: 30, weight: 160},
+        {name: "Anthony", age: 37, weight: 180}
       ]
     });
 
@@ -87,9 +138,16 @@ export default Ember.Route.extend({
       left: e("index * (1/table.length * 200 + 3)")
     });
 
+    var scatterMark = CircleMark.create({
+      radius: e("5"),
+      cy: e("element.age"),
+      cx: e("element.weight"),
+      fill: e("'#49B08D'")
+    });
+
     // TODO: move this binding to component or controller
     var compiler = MarksToD3Compiler.create({
-      marks: [rectMark],
+      marks: [rectMark, scatterMark],
       table: table,
       scalars: {}
     });
