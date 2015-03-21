ce1147ed2488e53b06e688d045d82a8ea0defe39
Anthony Sherbondy <ts1235813@gmail.com>
2015-03-20 23:31:08 -0600

    Instructions produce marks

--- app/models/instruction.js
+++ app/models/instruction.js
@@ -1,4 +1,5 @@
 import Ember from "ember";
+import RectangleMark from 'tukey/models/mark/rectangle-mark';
 
 export default Ember.Object.extend({
   operation: null,
@@ -22,6 +23,33 @@ export default Ember.Object.extend({
     return ret;
   }.property('subInstructions.@each.flattenedList'),
 
+  // Any loop or draw instruction should be able to return
+  // a list of marks
+  // TODO(Tony) not sure how we are going to handle adjust
+  // instructions
+  marks: function() {
+    var operation = this.get("operation");
+    var marks;
+    if (operation === "root" || operation === "loop") {
+      marks = this.get("subInstructions").getEach("marks");
+    } else if (operation === "draw") {
+      var attrs = this.get("attrs");
+      marks = [RectangleMark.create(attrs)];
+    } else {
+      console.log("should not be asked to mark this instruction");
+      return [];
+    }
+    // Flatten the marks before returning
+    return marks.reduce((prev, item) => {
+      if (Ember.isArray(item)) {
+        return prev.concat(item);
+      }
+      return prev.concat([item]);
+    }, []);
+  }.property(""),
+  //}.property("subInstrunctions.@each.marks"),
+
+  // TODO(Tony) These should go away now because we just have one table
   availableLoopVariables: function() {
     if (this.get("operation") === "root") {
       return [];
@@ -37,6 +65,7 @@ export default Ember.Object.extend({
     return ret;
   }.property("parentInstruction.availableLoopVariables", "operation", "loopVariable"),
 
+  // TODO(Tony) These should go away now because we just have one table
   loopVariable: function() {
     return this.get("loopData.name");
   }.property("loopData.name")
--- app/routes/marks.js
+++ app/routes/marks.js
@@ -4,6 +4,7 @@ import TableColumn from 'tukey/models/table-column';
 import Expression from 'tukey/models/expression';
 import RectangleMark from 'tukey/models/mark/rectangle-mark';
 import CircleMark from 'tukey/models/mark/circle-mark';
+import Instruction from "tukey/models/instruction";
 
 var e = Expression.e;
 
@@ -15,44 +16,95 @@ var MarksToD3Compiler = Ember.Object.extend({
   }.property("marks.[]", "marks.@each.d3Code")
 });
 
+function getMarks() {
+  var rectMark = RectangleMark.create({
+    width: e("1/table.length * scalars.canvasWidth"),
+    height: e("element.age"),
+    top: e("scalars.canvasHeight - element.age"),
+    left: e("index * (1/table.length * scalars.canvasWidth + scalars.padding)"),
+    opacity: e("0.3")
+  });
+
+  var scatterMark = CircleMark.create({
+    radius: e("5"),
+    cy: e("element.age"),
+    cx: e("element.weight"),
+    fill: e("'#49B08D'")
+  });
+  return [rectMark, scatterMark];
+}
+
+function getData() {
+  var scalars = [
+    Ember.Object.create({name: 'canvasHeight', value: 200}),
+    Ember.Object.create({name: 'canvasWidth', value: 200}),
+    Ember.Object.create({name: 'padding', value: 3})
+  ];
+  var columns = [
+      {name: "Nhan", age: 27, weight: 120},
+      {name: "Zack", age: 30, weight: 160},
+      {name: "Anthony", age: 37, weight: 180}
+  ];
+  var tableColumns = columns.map((hash) => {
+    return TableColumn.create().set("columnHash", hash);
+  });
+  var table =  Table.create({
+    columns: tableColumns
+  });
+  return {table, scalars};
+}
+
+function getInstructionTree() {
+  var drawOp = Instruction.create({
+      operation: "draw",
+      mark: "rect",
+      attrs: {
+        top: e("10"),
+        left: e("10"),
+        width: e("100"),
+        height: e("100"),
+        opacity: e("0.3")
+      },
+      markId: 1
+  });
+  var setOp = Instruction.create({
+      operation: "set",
+      property: "height",
+      propertyValue: 50,
+  });
+  drawOp.addSubInstruction(setOp);
+
+  var root = Instruction.create({
+      operation: "root",
+  });
+  root.addSubInstruction(drawOp);
+
+  return root;
+}
+
 export default Ember.Route.extend({
   model: function() {
-    var columns = [
-        {name: "Nhan", age: 27, weight: 120},
-        {name: "Zack", age: 30, weight: 160},
-        {name: "Anthony", age: 37, weight: 180}
-    ];
-    var tableColumns = columns.map((hash) => {
-      return TableColumn.create().set("columnHash", hash);
-    });
-    var table =  Table.create({
-      columns: tableColumns
-    });
 
-    var rectMark = RectangleMark.create({
-      width: e("1/table.length * scalars.canvasWidth"),
-      height: e("element.age"),
-      top: e("scalars.canvasHeight - element.age"),
-      left: e("index * (1/table.length * scalars.canvasWidth + scalars.padding)"),
-      opacity: e("0.3")
-    });
+    // The Data
+    var {table, scalars} = getData();
 
-    var scatterMark = CircleMark.create({
-      radius: e("5"),
-      cy: e("element.age"),
-      cx: e("element.weight"),
-      fill: e("'#49B08D'")
-    });
+    // The Instructions
+    var rootInstruction = getInstructionTree();
 
+    // The Marks
+    var marks = getMarks();
+
+    // TODO remove once instructions can calculate marks
+    //rootInstruction.set("marks", marks);
+
+    // The Marks Compiler
     // TODO: move this binding to component or controller
-    var compiler = MarksToD3Compiler.create({
-      marks: [rectMark, scatterMark],
+    var compiler = MarksToD3Compiler.extend({
+      marks: Ember.computed.alias("instructionTree.marks"),
+    }).create({
+      instructionTree: rootInstruction,
       table: table,
-      scalars: [
-        Ember.Object.create({name: 'canvasHeight', value: 200}),
-        Ember.Object.create({name: 'canvasWidth', value: 200}),
-        Ember.Object.create({name: 'padding', value: 3})
-      ]
+      scalars: scalars,
     });
 
     return compiler;
