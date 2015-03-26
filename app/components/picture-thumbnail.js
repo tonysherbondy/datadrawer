1425aa814294c81df8e6ba9bdf3fb8ad6670d9db
Zack Reynolds <reynolds.zack@gmail.com>
2015-03-25 17:43:46 -0700

    Scaled thumbnails

--- app/components/picture-thumbnail.js
+++ app/components/picture-thumbnail.js
@@ -4,35 +4,52 @@ import layout from '../templates/components/picture-thumbnail';
 export default Ember.Component.extend({
   layout: layout,
 
+  picture: Ember.required(),
+
+  wholeClass: Ember.computed(function() {
+    return "noselect " + this.get("svgClass");
+  }).property("picture.id"),
+
+  viewBox: "0 0 640 480",
+
+  svgClass: Ember.computed(function() {
+    return "picture-thumbnail-" + this.get("gClass");
+  }).property("picture.id"),
+
+  gClass: Ember.computed(function() {
+    return "baby-chart-" + this.get("picture.id");
+  }).property("picture.id"),
+
+  selectChart: function() {
+    return d3.select(this.$("." + this.get("gClass"))[0]);
+  },
+
   draw: function() {
     this.selectChart().remove();
 
-    d3.select("svg.draw-panel").append("g").attr("class", "chart");
+    d3.select("svg." + this.get("svgClass"))
+      .append("g").attr("class", this.get("gClass"));
 
-    // Make table and scalar data available to the chart, these are used inside attr
-    // functions
+    // Make table and scalar data available to the chart, these are used
+    // inside attr functions
     var table = [];
-    if (!Ember.isEmpty(this.get("table.columns"))) {
-      table = this.get("table.columns").map((column) => {
+    if (!Ember.isEmpty(this.get("picture.table.columns"))) {
+      table = this.get("picture.table.columns").map((column) => {
         return column.get("columnHash");
       });
     }
     var scalars = {};
-    if (!Ember.isEmpty(this.get("scalars"))) {
-      this.get("scalars").forEach((scalar) => {
+    if (!Ember.isEmpty(this.get("picture.scalars"))) {
+      this.get("picture.scalars").forEach((scalar) => {
         scalars[scalar.get("name")] = scalar.get("value");
       });
     }
 
     try {
-      eval(this.get("d3Code"));
+      eval(this.get("picture.d3Code"));
     } catch (error) {
       console.log("D3 CODE EVAL ERROR: " + error);
     }
-
-    this.setupAdjustListeners();
-    this.drawMarkControls();
-
-  }.observes("d3Code", "table.columns.@each.columnHash", "scalars.@each.value", "scalars.@each.name")
+  }.observes("picture.d3Code").on('didInsertElement')
 
 });
