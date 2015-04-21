5c88a6adb51ed16049e9f8d3d7540ba69e5156f9
Anthony Sherbondy <ts1235813@gmail.com>
2015-04-21 00:20:57 -0700

    sub-instructions show up in list

--- src/scripts/components/instructions/InstructionList.js
+++ src/scripts/components/instructions/InstructionList.js
@@ -8,21 +8,33 @@ export default class InstructionList extends React.Component {
   }
 
   render() {
+    let getInstructionItems = (instruction, index) => {
+      let subInstructions = instruction.instructions;
+      let subInstructionList;
+      if (subInstructions) {
+        subInstructionList = (
+          <ul className='sub-instructions-list'>
+            {subInstructions.map(getInstructionItems)}
+          </ul>
+        );
+      }
+
+      return (
+        <li className='instruction-list-item' key={index}>
+          {instruction.getUISentence()}
+          <button
+            type='button'
+            className='delete-instruction'
+            onClick={this.removeInstruction.bind(this, index)}>
+            <i className="fa fa-close"></i>
+          </button>
+          {subInstructionList}
+        </li>
+      );
+    };
     return (
       <ul className='instructions-list'>
-        {this.props.instructions.map((instruction, index) => {
-          return (
-              <li className='instruction-list-item' key={index}>
-                {instruction.getUISentence()}
-                <button
-                  type='button'
-                  className='delete-instruction'
-                  onClick={this.removeInstruction.bind(this, index)}>
-                  <i className="fa fa-close"></i>
-                </button>
-              </li>
-          );
-        })}
+        {this.props.instructions.map(getInstructionItems)}
       </ul>
     );
   }
