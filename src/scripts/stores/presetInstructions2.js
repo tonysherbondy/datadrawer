5990eb70ca1e3f9ce7b64dd71435b048fd69da10
Anthony Sherbondy <ts1235813@gmail.com>
2015-04-26 22:51:42 -0700

    Add lines to plot with whiskers

--- src/scripts/stores/presetInstructions2.js
+++ src/scripts/stores/presetInstructions2.js
@@ -58,6 +58,47 @@ const instructions = [
         strokeWidth: 2,
         from: {id: 'canvas', point: 'left'},
         to: {id: 'canvas', point: 'right'}
+      }),
+      new ScaleInstruction({
+        id: 'i7',
+        shape: {id: 'shape_i6', isLoop: true},
+        prop: 'x2',
+        point: 'right',
+        to: {id: 'swidth'}
+      }),
+      new MoveInstruction({
+        id: 'imove2',
+        shape: {id: 'shape_i6', isLoop: true},
+        point: 'center',
+        to: {id: 'shape_i4', point: 'center', isLoop: true}
+      }),
+      // TODO missing ability to draw a line centered about a point
+      new DrawLineInstruction({
+        id: 'i8',
+        strokeWidth: 2,
+        from: {id: 'shape_i6', point: 'right', isLoop: true},
+        width: 0,
+        height: 9.5
+      }),
+      new MoveInstruction({
+        id: 'imove3',
+        shape: {id: 'shape_i8', isLoop: true},
+        point: 'center',
+        to: {id: 'shape_i6', point: 'right', isLoop: true}
+      }),
+      // TODO missing ability to draw a line centered about a point
+      new DrawLineInstruction({
+        id: 'i9',
+        strokeWidth: 2,
+        from: {id: 'shape_i6', point: 'left', isLoop: true},
+        width: 0,
+        height: 9.5
+      }),
+      new MoveInstruction({
+        id: 'imove4',
+        shape: {id: 'shape_i9', isLoop: true},
+        point: 'center',
+        to: {id: 'shape_i6', point: 'left', isLoop: true}
       })
     ]
   })
