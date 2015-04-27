c912ad8dd31e0e194b1280f391bd086aa882e23d
Anthony Sherbondy <ts1235813@gmail.com>
2015-04-27 14:56:55 -0700

    Change utils name

--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -8,6 +8,13 @@ function rangeIter(start, end) {
 
 export default {
   rangeIter: rangeIter,
+
+  distanceBetweenPoints: function(pt1, pt2) {
+    let dx = pt2.x - pt1.x;
+    let dy = pt2.y - pt1.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+
   range: function(start, end) {
     return [...rangeIter(start, end)];
   }
