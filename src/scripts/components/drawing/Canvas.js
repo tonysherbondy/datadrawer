7fd5e0119df7ae753dd64131074b2623ec12989c
Anthony Sherbondy <ts1235813@gmail.com>
2015-05-01 12:38:27 -0700

    Can detect when we are close to a magnet

--- src/scripts/components/drawing/Canvas.js
+++ src/scripts/components/drawing/Canvas.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import _ from 'lodash';
+import {distanceBetweenPoints} from '../../utils/utils';
 
 class Canvas extends React.Component {
 
@@ -27,36 +29,61 @@ class Canvas extends React.Component {
     });
   }
 
-  drawMagnets() {
+  handleMagnetClick(id) {
+    let [, shapeName, pointName] = id.split('_');
+    console.log('draw from', shapeName, pointName);
+  }
+
+  // TODO - may want to just call this once and store in state
+  getMagnets() {
     // All shapes that are not the current editing shape
     // have magnets
     let editingInstruction = this.props.editingInstruction;
     if (!editingInstruction) {
       // Only draw magnets when we are currently drawing
-      return null;
+      return [];
     }
 
     let editingShapeName = editingInstruction.getShapeName();
-    function drawMagnet(magnet) {
+    return _.flatten(this.props.shapes
+            .filter(shape => shape.name !== editingShapeName)
+            .map(shape => shape.getMagnets()));
+  }
+
+  getCloseMagnets(point) {
+    // Return magnets that are within a threshold distance away from position
+    let threshold = 20;
+    return this.getMagnets().reduce((closeMagnets, magnet) => {
+      let d = distanceBetweenPoints(point, magnet);
+      return d < threshold ? closeMagnets.concat(magnet) : closeMagnets;
+    }, []);
+  }
+
+  drawMagnets() {
+    let drawMagnet = (magnet) => {
       let id = `magnet_${magnet.shapeName}_${magnet.pointName}`;
+      // TODO probably better way to handle this is to make magnet a component
       return (
-        <circle key={id} className='magnet' r='5' cx={magnet.x} cy={magnet.y} />
+        <circle onClick={this.handleMagnetClick.bind(this,id)} key={id} className='magnet' r='5' cx={magnet.x} cy={magnet.y} />
       );
+    };
+    return this.getMagnets().map(drawMagnet);
+  }
+
+  handleMouseMove(e) {
+    let {left, bottom, height} = this.refs.canvas.getDOMNode().getBoundingClientRect();
+    let x = e.clientX - left;
+    let y = e.clientY - (bottom - height);
+    let magnets = this.getCloseMagnets({x, y});
+    if (magnets.length > 0) {
+      let {shapeName, pointName} = magnets[0];
+      console.log(`found ${shapeName}'s ${pointName} close enough`);
     }
-    return this.props.shapes.filter(shape => shape.name !== editingShapeName)
-            .map(shape => {
-              return (
-                <g key={shape.name}>
-                  {shape.getMagnets().map(drawMagnet)}
-                </g>
-              );
-            });
   }
 
   render() {
-    console.log('props', this.props);
     return (
-      <svg className="canvas">
+      <svg ref='canvas' className="canvas" onMouseMove={this.handleMouseMove.bind(this)}>
         {this.drawShapes()}
         {this.drawMagnets()}
       </svg>
