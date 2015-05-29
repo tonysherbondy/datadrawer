dbf2acace8bb77544932b4ad0ee183beac6f2ea1
Ethan Sherbondy <sherbondye@gmail.com>
2015-05-28 20:01:03 -0400

    Proof of concept csv upload data variable rows.
    Things to address:
    - We need non-numerical data to be representable (namely strings)
      for the sake of labeling... And hopefully dates at some point soon.
    - Needs to be MUCH more robust, e.g. handling both row-based and
      column-based tabular data without a hitch (maybe papaparse gives
      us this for free).
    - Need to verify that duplicate row names are handled properly.
      No "uniquification" or unique-ness checks are done currently.
    
    ... and probably much more.

--- src/scripts/components/instructions/DataTable.js
+++ src/scripts/components/instructions/DataTable.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import VariablePill from '../VariablePill';
+import Papa from 'papaparse';
+
+import DataVariableActions from '../../actions/DataVariableActions';
+import DataVariableStore from '../../stores/DataVariableStore';
 
 export default class DataTable extends React.Component {
 
@@ -40,10 +44,60 @@ export default class DataTable extends React.Component {
             {rowElements}
           </tbody>
         </table>
+
+        <input
+          type='file'
+          onChange={this.handleUpload.bind(this)}
+          value='Upload CSV' />
       </div>
     );
   }
 
+  handleUpload(e) {
+    console.log('UPLOAD CSV...');
+    let file = e.target.files[0];
+
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+
+      complete: (results, f) => {
+        // currently assuming columns are keyed by first row
+        console.log('parsing complete: ', f, results);
+        var rowMap = new Map();
+        // papaparse makes sure all elements have all keys, even
+        // if vals are empty
+        for (let row of results.data) {
+          for (let key of Object.keys(row)) {
+            // value is an array of row values
+            if (rowMap.has(key)) {
+              var val = rowMap.get(key);
+              val.push(row[key]);
+              rowMap.set(key, val);
+            } else {
+              rowMap.set(key, [row[key]]);
+            }
+          }
+        }
+
+        rowMap.forEach((value, key, rm) => {
+          console.log(rowMap === rm);
+          let jsonVal = JSON.stringify(value);
+          console.log('json val:', jsonVal);
+          let variable = DataVariableStore.generateNewVariable({
+            name: key,
+            isRow: true,
+            definition: jsonVal
+          });
+          DataVariableActions.appendVariable(variable);
+        });
+      },
+
+      error: (error, f) => {
+        console.log('parse error: ', f, error);
+      }
+    });
+  }
 }
 
 DataTable.propTypes = {
