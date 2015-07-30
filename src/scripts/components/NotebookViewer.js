f741f38b06ac9874d7b7b12b9dccf2e68c369a29
Anthony Sherbondy <ts1235813@gmail.com>
2015-07-30 13:06:21 -0700

    Can edit picture from view mode

--- src/scripts/components/NotebookViewer.js
+++ src/scripts/components/NotebookViewer.js
@@ -4,13 +4,29 @@ import Canvas from './drawing/Canvas';
 class NotebookViewer extends React.Component {
   render() {
     return (
+      <div className='notebook-viewer-container'>
         <Canvas
           className='canvas view'
           activePicture={this.props.activePicture}
           drawingMode='normal'
           shapes={this.props.shapes} />
+        <div
+          onClick={this.handleEditClick.bind(this)}
+          className="return-to-edit-popover">
+          Edit
+        </div>
+      </div>
     );
   }
+
+  handleEditClick() {
+    let {notebookId, pictureId} = this.context.router.getCurrentParams();
+    this.context.router.transitionTo(`/notebook/${notebookId}/picture/${pictureId}/edit`);
+  }
 }
+
+NotebookViewer.contextTypes = {
+  router: React.PropTypes.func.isRequired
+};
 
 export default NotebookViewer;
